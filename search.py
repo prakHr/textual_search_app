c544546afd68ba0c21008d5ebfb1ea4dc82a1de0
Prakhar Gandhi <gprakhar0@gmail.com>
2024-11-05 22:02:22 +0530

    Added app

--- search.py
+++ search.py
@@ -19,7 +19,6 @@ from googletrans import Translator
 import logging
 import subprocess
 
-
 from textual.app import App, ComposeResult
 from textual.containers import Vertical, VerticalScroll
 from textual.widgets import Input, Button, Static
