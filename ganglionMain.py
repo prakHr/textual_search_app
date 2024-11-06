664c69900977b0974ce45bf8178bb89d854524c7
Prakhar Gandhi <gprakhar0@gmail.com>
2024-11-06 19:10:01 +0530

    Added CI/CD Pipeline again again and again...

--- ganglionMain.py
+++ ganglionMain.py
@@ -1,6 +1,7 @@
 import subprocess
 if __name__=="__main__":
     command = ["textual-web", "--config","ganglion.toml"]
+    print("REACHED HERE____")
     # process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     # Run the command in the foreground and wait for it to complete
     ans = subprocess.call(command, shell=True)
